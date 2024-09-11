a864c9c3f3617b9a3425395ae70c8f54e31bb47b
Nikita Manjhi <nikitamanjhi540@gmail.com>
2024-09-12 00:21:29 +0530

    changed navbar ui

--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -1,46 +1,107 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import { Badge, Button } from "@material-tailwind/react";
-import { CiUser,CiSearch,CiHeart,CiShoppingCart,CiMenuBurger} from "react-icons/ci";
-import {IconContext} from "react-icons"
-import { useSelector } from 'react-redux';
-const Navbar = () => {
-    const quantity = useSelector(state=>state.cart.quantity);
-  return (
-    <div className='flex h-16 shadow-lg '>
-        <div className='flex items-center justify-between mx-24 px-1 py-2 h-full w-full '>
-            <div className='flex items-center gap-5 mx-10'>
-                <div className=''><IconContext.Provider value={{ size :"25"}}><CiMenuBurger /></IconContext.Provider></div>
-                <div className='text-3xl font-semibold tracking-wide'>PRAGO</div>
-            </div>
-            <div className='flex items-center  w-full'>
-                <ul className='flex items-center gap-14  w-full  text-xl font-normal'>
-                  
-                    <Link to={"/"}>
-                    <li>Home</li>
-                    </Link>
-                    <li>Shop</li>
-                    <li>Women</li>
-                    <li>Men</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            <div className='flex items-center justify-center '>
-                <ul className='flex items-center justify-center gap-5'>
-                    <IconContext.Provider value={{size:"25"}}>
-                    <li><CiUser/></li>
-                    <li><CiSearch/></li>
-                    <li><Badge content="0"><CiHeart/></Badge></li>
-                    <Link to={"/cart"}>
-                    <li><Badge content={quantity}><CiShoppingCart/></Badge></li>
-                    </Link>
-                  
-                    </IconContext.Provider>
-                </ul>
-            </div>
-        </div>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
-export default Navbar
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+  const quantity = useSelector((state) => state.cart.quantity);
+
+  return (
+    <div className="fixed top-0 left-0 right-0 z-50 bg-white shadow-md">
+      <div className="flex items-center justify-between py-5 font-medium px-6">
+        <h1 className="text-3xl">PRAGO</h1>
+
+        {/* Desktop Nav Links */}
+        <div className="flex-grow flex justify-center">
+          <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+            <NavLink to="/" className="flex flex-col items-center gap-1">
+              <p className="text-lg">Home</p>
+              <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+            </NavLink>
+            <NavLink to="/products" className="flex flex-col items-center gap-1">
+              <p className="text-lg">Collections</p>
+              <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+            </NavLink>
+            <NavLink to="/about" className="flex flex-col items-center gap-1">
+              <p className="text-lg">About</p>
+              <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+            </NavLink>
+            <NavLink to="/contact" className="flex flex-col items-center gap-1">
+              <p className="text-lg">Contact</p>
+              <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+            </NavLink>
+          </ul>
+        </div>
+
+        {/* Icons */}
+        <div className="flex items-center gap-6">
+          <img
+            src={assets.search_icon}
+            alt="Search"
+            className="w-5 cursor-pointer"
+          />
+          <div className="group relative">
+            <img
+              src={assets.profile_icon}
+              alt="Profile"
+              className="w-5 cursor-pointer"
+            />
+            <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+              <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+                <p className="cursor-pointer hover:text-black">My Profile</p>
+                <p className="cursor-pointer hover:text-black">Orders</p>
+                <p className="cursor-pointer hover:text-black">Logout</p>
+              </div>
+            </div>
+          </div>
+          <Link to="/cart" className="relative">
+            <img src={assets.cart_icon} alt="Cart" className="w-5 min-w-5" />
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+              {quantity}
+            </p>
+          </Link>
+
+          <img
+            src={assets.menu_icon}
+            alt="Menu"
+            className="w-5 cursor-pointer sm:hidden"
+            onClick={() => setVisible(true)}
+          />
+        </div>
+      </div>
+
+      {/* Sidebar menu for small screens */}
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            className="flex items-center gap-4 p-3 cursor-pointer"
+            onClick={() => setVisible(false)}
+          >
+            <img src={assets.dropdown_icon} alt="Back" className="h-4 rotate-180" />
+            <p>BACK</p>
+          </div>
+          <NavLink to="/" className="py-2 pl-6 border font-semibold">
+            HOME
+          </NavLink>
+          <NavLink to="/products" className="py-2 pl-6 border">
+            COLLECTIONS
+          </NavLink>
+          <NavLink to="/about" className="py-2 pl-6 border">
+            ABOUT
+          </NavLink>
+          <NavLink to="/contact" className="py-2 pl-6 border">
+            CONTACT
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
